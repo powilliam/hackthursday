68320b1898567eac1123e5df88b6ad15fd83d3b6
William Porto <william.porto@nave.rs>
2022-03-23 05:40:44 -0300

    chore(babel): adding domain to module-resolver

--- rn-app/babel.config.js
+++ rn-app/babel.config.js
@@ -9,6 +9,7 @@ module.exports = function (api) {
           alias: {
             ui: "./src/ui",
             data: "./src/data",
+            domain: "./src/domain",
           },
         },
       ],
