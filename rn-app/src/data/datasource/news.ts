fa1e47c845790e918ce61d80282ae5c5381e0581
William Porto <william.porto@nave.rs>
2022-03-22 23:58:04 -0300

    feature(data): implementing datasources and repositories

--- rn-app/src/data/datasource/news.ts
+++ rn-app/src/data/datasource/news.ts
@@ -1,22 +1,41 @@
-import { AxiosInstance } from "axios";
+import { AxiosInstance, AxiosResponse } from "axios";
 
 import NewsService from "data/service/news";
+import { Source } from "data/entity/source";
+import { Article } from "data/entity/article";
+
+export interface GetSourceHeadlinesDTO {
+  readonly status: string;
+  readonly totalResults: number;
+  readonly articles: Article[];
+}
+
+export interface GetAvailableSourcesDTO {
+  readonly status: string;
+  readonly sources: Source[];
+}
 
 export interface NewsRemoteDataSource {
-  // TODO: Create a DTO for this request
-  getSourceHeadlines(sources: string): Promise<{}>;
-  // TODO: Create a DTO for this request
-  getAvailableSources(): Promise<{}>;
+  getSourceHeadlines(
+    sources: string
+  ): Promise<AxiosResponse<GetSourceHeadlinesDTO>>;
+  getAvailableSources(): Promise<AxiosResponse<GetAvailableSourcesDTO>>;
 }
 
 export function newsRemoteDataSourceFactory(
   newsService: AxiosInstance = NewsService
 ): NewsRemoteDataSource {
+  const ENDPOINTS = {
+    HEADLINES: "/v2/top-headlines",
+    SOURCES: "/v2/top-headlines/sources",
+  };
+
   return {
-    // TODO: Implement this method
     getSourceHeadlines: (sources) =>
-      newsService.get("", { params: { sources } }),
-    // TODO: Add available categories
-    getAvailableSources: async () => ({}),
+      newsService.get<GetSourceHeadlinesDTO>(ENDPOINTS.HEADLINES, {
+        params: { sources },
+      }),
+    getAvailableSources: () =>
+      newsService.get<GetAvailableSourcesDTO>(ENDPOINTS.SOURCES),
   };
 }
--- rn-app/src/data/repository/news.ts
+++ rn-app/src/data/repository/news.ts
@@ -2,20 +2,25 @@ import {
   newsRemoteDataSourceFactory,
   NewsRemoteDataSource,
 } from "data/datasource/news";
+import { Source } from "data/entity/source";
+import { Article } from "data/entity/article";
 
-// TODO: it shouldn't extends a data source
 export interface NewsRepository {
-  // TODO: Use Source and Headline entity
-  getSourceHeadlines(source: {}): Promise<[]>;
-  // TODO: Use Source entitiy
-  getAvailableSources(): Promise<any[]>;
+  getSourceHeadlines(source: Source): Promise<Article[]>;
+  getAvailableSources(): Promise<Source[]>;
 }
 
 export function newsRepositoryFactory(
   newsRemoteDataSource: NewsRemoteDataSource = newsRemoteDataSourceFactory()
 ): NewsRepository {
   return {
-    getSourceHeadlines: async (source) => [],
-    getAvailableSources: async () => [],
+    getSourceHeadlines: async (source) => {
+      const { data } = await newsRemoteDataSource.getSourceHeadlines(source.id);
+      return data?.articles || [];
+    },
+    getAvailableSources: async () => {
+      const { data } = await newsRemoteDataSource.getAvailableSources();
+      return data?.sources || [];
+    },
   };
 }
